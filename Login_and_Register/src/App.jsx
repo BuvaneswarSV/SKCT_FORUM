ce157562196c82dfe62a74a344fcabefadc9358b
Buvaneswar <125265843+BuvaneswarSV@users.noreply.github.com>
2024-07-25 10:36:46 +0530

    Login and Register

---
+++ Login_and_Register/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Login from './pages/login/Login'
+import Register from './pages/register/Register'
+
+function App() {
+  return (
+    <div>
+      {/* <Login/> */}
+      <Register/>
+    </div>
+    // <Routes>
+    //   <Route/>
+
+    //   <Route/>
+    // </Routes>
+  )
+}
+
+export default App
---
+++ Login_and_Register/src/components/button/Buttons.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+
+function Buttons() {
+  return (
+    <Button variant="contained">Submit</Button>
+  )
+}
+
+export default Buttons
